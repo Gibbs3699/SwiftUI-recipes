be1b133debb153022ff02dd7b6d3365124be79cb
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-06-22 23:21:29 +0700

    Adding MVVM and Data Flow

---
+++ MVVM/MVVM/MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by TheGIZzz on 22/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVVM/MVVM/MVVM/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  MVVM
+//
+//  Created by TheGIZzz on 22/6/2565 BE.
+//
+
+import Foundation
+
+struct Post: Decodable {
+    let id: Int
+    let title: String
+    let body: String
+}
